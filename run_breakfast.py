e8426a79b52b1303efb62acee66239d65d434c4a
Andrew S <andrew.senkowski@zingermans.com>
2026-03-16 12:58:57 -0400

    Now automated. .gitignore and standalone SQL Server access added.

--- run_breakfast.py
+++ run_breakfast.py
@@ -2,9 +2,13 @@ import breakfast_sheet
 import time
 
 if __name__ == '__main__':
-    monthyear = time.strftime('%B_%Y')
-    date = time.strftime('%Y%m%d')
-    breakfast_sheet.generate_daily_sheet(monthyear, date)
+    try:
+        monthyear = time.strftime('%B_%Y')
+        date = time.strftime('%Y%m%d')
+        breakfast_sheet.generate_daily_sheet(monthyear, date)
+    except Exception as e:
+        with open('./error.txt', 'a') as errfile:
+            errfile.write(f'{time.strftime('%m_%d_%y %H:%M:%S')}: {e}\n')
 
 # for i in range(1, 15):
 #     day = f'2026030{i}' if i < 10 else f'202603{i}'
